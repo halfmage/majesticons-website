08bbe68b27528f9a8decc8e1c8ce910812793411
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2021-05-26 11:14:29 +0200

    general website updates

--- _data/majesticons.js
+++ _data/majesticons.js
@@ -1,19 +1,22 @@
 const fg = require("fast-glob");
 const path = require("path");
 const fs = require("fs");
+
 module.exports = function () {
   return new Promise(async (resolve, reject) => {
-    const iconFolder = path.resolve(__dirname, "node_modules/majesticons/icons");
+    const iconFolder = path.resolve(__dirname, "../node_modules/majesticons/icons");
     const svgIcons = await fg("**/*.svg", { cwd: iconFolder });
     const iconData = await Promise.all(
       svgIcons.map(async (name) => {
-        const namePlain = path.parse(name).name;
+        const nameWithoutExtension = path.parse(name).name;
         const source = fs.readFileSync(path.resolve(iconFolder, name), {
           encoding: "utf8",
         });
-        return { name, source, namePlain };
+        const namePlain = path.parse(name).name;
+        const sourceWithClass = source.replace("<svg", '<svg class="w-6 lg:w-8 h-6 lg:h-8"');
+        return { name, source, sourceWithClass, nameWithoutExtension, namePlain };
       })
     );
-    resolve(JSON.stringify(iconData));
+    resolve(iconData);
   });
 };
