265fc1907dc9286935049f4b130a194536ae5230
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2021-08-08 21:00:55 +0200

    added search page

--- .eleventy.js
+++ .eleventy.js
@@ -27,6 +27,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addWatchTarget('./_tmp/style.css')
   eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' })
+  eleventyConfig.addPassthroughCopy({ './node_modules/alpinejs/dist/alpine.js': './js/alpine.js' })
 
   eleventyConfig.addShortcode('version', function () {
     return now
