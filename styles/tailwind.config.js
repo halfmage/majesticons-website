cc03fb31c3ecf3b5cca1338bc1b861a3aab010b6
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2024-07-28 21:23:21 +0200

    first round

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -1,23 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  darkMode: 'class',
   content: ["./_site/**/*.{html,css,js}"],
-  theme: {
-    extends: {
-      colors: {
-        white: '#fff',
-        black: '#000',
-        gray: colors.zinc,
-        amber: colors.amber,
-        orange: colors.orange,
-        blue: colors.blue,
-      },
-      fontFamily: {
-        'primary': ['Plus Jakarta Sans, system-ui, sans-serif'],
-      },
-    }
-  },
   variants: {},
   plugins: [],
 }
